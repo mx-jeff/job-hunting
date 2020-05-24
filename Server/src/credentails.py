5800e94453cb1380f1e35cc18b77d0bd4d1fd3eb
Jeferson <mx,jeferson.12@gmail.com>
2020-05-24 20:20:22 -0300

    Revert "add test file"
    This reverts commit 5147bd5ffa49a0cf1a2b50fd571e52ecd11f920a.

---
+++ Server/src/credentails.py
@@ -0,0 +1,10 @@
+"""
+Credentials Infojobs here, replace the following line below
+"""
+import os
+
+user = os.environ.get('USER') or "insert your username here..."
+password = os.environ.get('PASSWORD') or "insert your password here..."
+
+vagasUser = os.environ.get('VAGAS_USER') or "insert your username here..."
+vagasPassword = os.environ.get('VAGAS_PASSWORD') or "insert your password here..."
