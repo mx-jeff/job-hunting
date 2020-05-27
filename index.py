6abb9eae88cdc07872d7c85edb615f12d801819e
Jeferson <mx,jeferson.12@gmail.com>
2020-05-27 00:05:10 -0300

    change input

--- index.py
+++ index.py
@@ -1,18 +1,20 @@
 from src.controllers.infojobsController import searchInfojob
 from src.controllers.vagasComController import searchVagasCom
-import sys
 
 
 def main():
     try:
-        option = sys.argv[1]
-        job = str(sys.argv[2]).lower()
-        str(option).lower()
+        print("~" * 40)
+        print(str("BUSCADOR DE EMPREGOS").center(40))
+        print("~" * 40)
+
+        option = str(input("Digite o site de busca: (infojobs ou vagas.com) ")).lower().strip()
+        job = str(input('Selecione a vaga desejada: '))
 
         if option == "infojobs":
             searchInfojob(job)
 
-        elif option == "vagascom":
+        elif option == "vagas.com":
             searchVagasCom(job)
         
         else:
