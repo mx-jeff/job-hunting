8e0fb31d41c6605f11c620a60a5a962757126d8e
Jeff <mx.jeferson.12@gmail.com>
2020-06-10 23:24:28 -0300

    add tasklist

--- Client/job-hunt.py
+++
@@ -1,9 +0,0 @@
-from os import system 
-
-jobsSite = ["infojobs", "vagascom"]
-jobs = ["python","php","javascript","front-end","home office ti"]
-
-
-for jobSite in jobsSite:
-    for job in jobs:
-        system(f"heroku run -a jober-hunt python index.py {jobSite} {job}")
---
+++ tasklist.md
@@ -0,0 +1,8 @@
+[x] - Infojobs
+[x] - vagas.com
+[x] - cloud deploy
+[x] - automate scripts
+[ ] - create GUI
+[ ] - linkedin
+[ ] - github
+[ ] - trampos.com
