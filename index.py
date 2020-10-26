bfa2d2caff0b09611ddfa88467037573e0b1e0ed
Jeferson <mx,jeferson.12@gmail.com>
2020-10-26 13:36:38 -0300

    change to master

--- index.py
+++ index.py
@@ -3,6 +3,26 @@ from src.controllers.vagasComController import searchVagasCom
 
 
 def main():
+    """
+        try:
+        option = sys.argv[1]
+        job = str(sys.argv[2]).lower()
+        str(option).lower()
+
+        if option == "infojobs":
+            searchInfojob(job)
+
+        elif option == "vagascom":
+            searchVagasCom(job)
+
+        else:
+            print('Insira um site de vagas!')
+
+    except IndexError:
+        print('Insira um site de vagas!')
+    :return:
+    """
+
     try:
         print("~" * 40)
         print(str("BUSCADOR DE EMPREGOS").center(40))
