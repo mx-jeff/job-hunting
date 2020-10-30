dbbc887cc39fe9cf578b03105c2f8e7eff580a93
Jeferson <mx,jeferson.12@gmail.com>
2020-10-30 15:20:38 -0300

    debug

--- src/config.py
+++ src/config.py
@@ -7,11 +7,11 @@ def setSelenium(link):
     options = Options()
     options.binary_location = environ.get('GOOGLE_CHROME_BIN')
     options.add_argument('--disable-notifications')
-    #options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-logging")
     options.add_experimental_option("detach", True)
+    options.add_argument('--headless')
     path = environ.get("CHROMEDRIVER_PATH") or "./chromedriver.exe"
 
     driver = webdriver.Chrome(path, options=options)
