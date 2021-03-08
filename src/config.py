d4e5888b9acb8b73fe94c726f5d05ff934f6aeea
Jeferson <mx,jeferson.12@gmail.com>
2021-03-08 11:57:20 -0300

    update searchbar

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ def setSelenium(link):
     options.add_experimental_option("detach", True)
     options.add_argument('--headless')
 
-    path = "./chromedriver.exe" or environ.get('CHROMEDRIVER_PATH')
+    path = environ.get('CHROMEDRIVER_PATH') or "./chromedriver.exe" 
 
     driver = webdriver.Chrome(path, options=options)
     driver.get(link)
