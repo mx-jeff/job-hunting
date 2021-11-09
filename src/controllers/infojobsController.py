5f230a1a3757325823acf7bf1620ee8046b42a8f
Jeff <mx.jeferson.12@gmail.com>
2021-11-09 12:41:28 -0300

    add jobhunting library

--- src/controllers/infojobsController.py
+++ src/controllers/infojobsController.py
@@ -1,25 +1,75 @@
-from src.Models.Infojobs import Infojobs
+from jobhunting import Infojobs
 from src.utils.output import output, checkBtn
 import eel
 
 
 def searchInfojob(jobTarget, login, password):
-    jobs = Infojobs()
+    """
+    Infojobs automatic subscription job
+
+    :jobTarget: target job to subsscribe
+    :login: infojobs user to login
+    :password: password to login
+    """
+    jobs = Infojobs(headless=True)
+    site_job = jobs.appName
+    job_type = jobTarget
 
     try:
-        jobs.login(login, password)
-        jobs.searchList(jobTarget)
+        output(jobs, f'{site_job} Iniciando...')
+        output(jobs, f'{site_job} Tentando logar...')
+
+        if not jobs.login(login, password):
+            output(jobs, f"{site_job} Login inválido ou campos errados!")
+            jobs.quitSearch()
+            return
+
+        output(jobs, f'{site_job} Selecionando vaga...')
+        jobs.searchList(job_type)
+        output(jobs, f'{site_job} Feito!, buscando vagas para {jobTarget}')
+
+        output(jobs, f'{site_job} Ajustando opções...')
         jobs.searchOptions()
+        output(jobs, f"{site_job} Feito!")
+
+        output(jobs, f'{site_job} Selecionando vagas disponiveis...')
         jobs.getJob()
+        output(jobs, f'{site_job} {len(jobs.jobsLink)} Vagas selecionadas!')
+
+        success = 0
+        fail = 0
+        output(jobs, f"{site_job} Se inscrevendo nas vagas...")
+
+        try:
+            for index, target in enumerate(jobs.jobsLink):
+                if target.startswith("https://") or target.startswith("http://"):
+                    status = jobs.subscribeJob(target)
+                    if status == "Vaga cadastrada!":
+                        success += 1
+
+                    else:
+                        fail += 1
+
+                    output(jobs, f"{site_job} {index + 1} vaga, status: {status}")
+        
+        except Exception:
+            output(jobs, f"{site_job} Erro ao se cadastrar, saindo...")
+
         jobs.quitSearch()
+
+        output(jobs, f'{site_job} Vagas inscritas: {success}')
+        output(jobs, f'{site_job} Vagas ja inscritas anteriomente ou requer preenchimento adicional: {fail}')
+        output(jobs, f"{site_job} Saindo... volte sempre :)")
+        checkBtn()
 
     except Exception as error:
-        output("Algum problema ocorreu e/ou as inforamções estão erradas!")
-        output(f"Erro {error}, contate o adminstrador do sistema")
-        checkBtn()
         jobs.quitSearch()
+        output(jobs, "Algum problema ocorreu e/ou as inforamções estão erradas!")
+        output(jobs, f"Erro {error}, contate o adminstrador do sistema")
+        output(jobs, f"{site_job} Saindo... volte sempre :)")
+        checkBtn()
 
     except KeyboardInterrupt:
-        output('Saindo, volte sempre!')
+        output(jobs, f"{site_job} Saindo... volte sempre :)")
         jobs.quitSearch()
         checkBtn()
--- src/controllers/vagasComController.py
+++ src/controllers/vagasComController.py
@@ -1,28 +1,74 @@
 from time import sleep
 
-from src.Models.vagasCom import VagasCom
+from jobhunting import VagasCom
 from src.utils.output import output, checkBtn
 
 import eel
 
 
 def searchVagasCom(targetJob, login, password):
-    vagas = VagasCom()
+    vagas = VagasCom(headless=True)
+    job_site = vagas.appName
+
+    output(vagas, f'{job_site} Iniciando...')
     try:
-        vagas.login(login, password)
+        output(vagas, f'{job_site} Tentando logar...')
+        if not vagas.login(login, password):
+            vagas.quitSearch()
+            output(vagas, f'{job_site} Login inválido ou campos errados!')
+            output(vagas, "[Vagas.com] Saindo... volte sempre :)")
+            return 
+
+        output(vagas, f'{job_site} logado com sucesso!')
+
+        output(vagas, f'{job_site} A selecionar vaga...')
         vagas.insertJob(targetJob)
+        output(vagas, f'{job_site} Vaga selecionada!')
+
+        output(vagas, f'{job_site} A ajustar opções...')
         vagas.searchOptions()
+        output(vagas, f'{job_site} Feito!')
+
+        output(vagas, f'{job_site} Listando Vagas...')
         vagas.selectJobs()
-        vagas.subscribeJob()
-        vagas.quitSearch()
+        output(vagas, f'{job_site} Feito!')
+        output(vagas, f"{len(vagas.targetLink)} vagas encontradas!")
+
+        success = 0
+        fail = 0
+        output(vagas, f"{job_site} Se inscrevendo nas vagas...")
+
+        try:
+            for index, target in enumerate(vagas.targetLink):
+                if target.startswith("https://") or target.startswith("http://"):
+                    status = vagas.subscribeJob(target)
+                    if status == "Vaga cadastrada!":
+                        success += 1
+
+                    else:
+                        fail += 1
+
+                    output(vagas, f"{job_site} {index + 1} vaga, status: {status}")
+
+        except Exception:  
+            output(vagas, f"{job_site} erro ao se inscrever!")
+
+        finally:
+            vagas.quitSearch()
+
+        output(vagas, f'{job_site} Vagas inscritas: {success}')
+        output(vagas, f'{job_site} Vagas ja inscritas anteriomente ou requer preenchimento adicional: {fail}')
+        output(vagas, f"{job_site} Saindo... volte sempre :)")
+        checkBtn()
 
     except Exception as error:
-        output("Algum problema ocorreu e/ou as informações estão erradas!")
         vagas.quitSearch()
+        output(vagas, "Algum problema ocorreu e/ou as informações estão erradas!")
+        output(vagas, f"{job_site} Saindo... volte sempre :)")
         checkBtn()
 
     except KeyboardInterrupt:
-        output('Saindo, volte sempre!')
         vagas.quitSearch()
+        output(vagas, f"{job_site} Saindo... volte sempre :)")
         checkBtn()
         
